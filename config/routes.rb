165abdffbe8a0b1047f2ded19e0e56852b1ee525
Olivor Hill <olivorhill@hotmail.com>
2024-09-25 00:34:08 +0100

    Completed Rails Task Manager. Could do with tidying up and CSS styling adding

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Read all (GET)
+  get "tasks", to: "tasks#index"
+
+  # Create (POST)
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # Read One (GET)
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Update / Edit (PATCH)
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete / Destroy (DELETE)
+  delete "tasks/:id", to: "tasks#destroy"
+
+  # If you look above at the first letter of each of the comments
+  # It spells out... you guessed it! CRUD!
+  # These are: Create, Update, Patch, Delete!
 end
